061a7bcfeda864ba67d60f06700c92c3ef00cbd3
caitlyn-wilhelm <cwilhelm@uw.edu>
2021-07-27 14:17:39 -0700

    fixed setup not matching other setup scripts

--- setup.py
+++ setup.py
@@ -1,51 +1,55 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from __future__ import absolute_import, division, print_function, unicode_literals
-import warnings
+from setuptools import setup
 import os
-import io
-import re
-
-try:
-    from setuptools import setup
-    from setuptools.command.install import install
-
-    setup
-except ImportError:
-    from distutils.core import setup
-
-    setup
-
-# Get the long description from the README
-def readme():
-    with open("README.md") as f:
-        return f.read()
 
 
-# Read, version funcs taken from:
-# https://github.com/ellisonbg/altair/blob/master/setup.py
-def read(path, encoding="utf-8"):
-    path = os.path.join(os.path.dirname(__file__), path)
-    with io.open(path, encoding=encoding) as fp:
-        return fp.read()
+# from __future__ import absolute_import, division, print_function, unicode_literals
+# import warnings
+# import os
+# import io
+# import re
 
 
-def version(path):
-    """
-    Obtain the packge version from a python file e.g. pkg/__init__.py
-    See <https://packaging.python.org/en/latest/single_source_version.html>.
-    """
-    version_file = read(path)
-    version_match = re.search(
-        r"""^__version__ = ['"]([^'"]*)['"]""", version_file, re.M
-    )
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
 
-
-VERSION = version("vspace/__init__.py")
+# try:
+#     from setuptools import setup
+#     from setuptools.command.install import install
+#
+#     setup
+# except ImportError:
+#     from distutils.core import setup
+#
+#     setup
+#
+# # Get the long description from the README
+# def readme():
+#     with open("README.md") as f:
+#         return f.read()
+#
+#
+# # Read, version funcs taken from:
+# # https://github.com/ellisonbg/altair/blob/master/setup.py
+# def read(path, encoding="utf-8"):
+#     path = os.path.join(os.path.dirname(__file__), path)
+#     with io.open(path, encoding=encoding) as fp:
+#         return fp.read()
+#
+#
+# def version(path):
+#     """
+#     Obtain the packge version from a python file e.g. pkg/__init__.py
+#     See <https://packaging.python.org/en/latest/single_source_version.html>.
+#     """
+#     version_file = read(path)
+#     version_match = re.search(
+#         r"""^__version__ = ['"]([^'"]*)['"]""", version_file, re.M
+#     )
+#     if version_match:
+#         return version_match.group(1)
+#     raise RuntimeError("Unable to find version string.")
+#
+#
+# VERSION = version("vspace/__init__.py")
 
 # Setup!
 setup(
@@ -66,6 +70,10 @@ setup(
     author_email="cwilhelm@uw.edu",
     license="MIT",
     packages=["vspace"],
+    use_scm_version={
+        "write_to": os.path.join("vspace", "vspace_version.py"),
+        "write_to_template": '__version__ = "{version}"\n',
+    },
     entry_points={"console_scripts": ["vspace=vspace.vspace:main",],},
     install_requires=["numpy", "matplotlib", "argparse",],
     include_package_data=True,
